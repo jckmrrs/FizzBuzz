4d6df3df0c8eae6cebc05558d8eae6fd3ae00859
Jack <jack3009@hotmail.co.uk>
2020-10-08 16:36:46 +0100

    Fizzbuzz and spec completed

--- Lib/fizzbuzz.rb
+++ Lib/fizzbuzz.rb
@@ -1,3 +1,17 @@
-def fizzbuzz (number)
-		'fizz'
+def fizzbuzz(number)
+
+    for number in 0..number do
+        if number == 0
+            puts number
+        elsif number % 15 == 0
+            puts "fizzbuzz"
+        elsif number % 3 == 0
+            puts "fizz"
+        elsif number % 5 == 0
+            puts "buzz"
+        else
+            puts number
+        end
+    end
 end
+
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,3 +5,44 @@ describe 'fizzbuzz' do
 		end
 end
 
+describe 'fizzbuzz' do
+    it 'returns "buzz" when passed 5' do
+        expect(fizzbuzz(5)).to eq 'buzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "fizzbuzz" when a number is divisible by 3 and 5' do
+        expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns the number given if not 3, 5 or 15' do
+        expect(fizzbuzz(2)).to eq 2
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "fizz" when a number is divisible by 3' do
+            expect(fizzbuzz(6)).to eq 'fizz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "buzz" when a number is divisible by 5' do
+            expect(fizzbuzz(10)).to eq 'buzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns "fizzbuzz" when a number is divisible by 3 and 5' do
+            expect(fizzbuzz(60)).to eq 'fizzbuzz'
+    end
+end
+
+describe 'fizzbuzz' do
+    it 'returns number when a number is 0' do
+        expect(fizzbuzz(0)).to eq number
+    end
+end
